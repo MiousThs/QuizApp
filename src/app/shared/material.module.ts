adb87962755c968ecc39d94ed060b540af0c14fc
miousThs <mious.ths@gmail.com>
2020-03-08 00:39:37 +0200

    set up main navigation bars

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 const modules = [
@@ -17,8 +18,9 @@ const modules = [
   MatButtonModule,
   MatSidenavModule,
   MatIconModule,
-  MatListModule
-]
+  MatListModule,
+  MatDividerModule
+];
 
 @NgModule({
   declarations: [],
