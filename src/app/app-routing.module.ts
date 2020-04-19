64307320fab7172f3c2d5735317811a85263d913
miousThs <mious.ths@gmail.com>
2020-04-19 12:38:27 +0300

    init subrouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'dashboard', redirectTo: 'process' },
+    { path: 'process', loadChildren: () => import('./quiz-process/quiz-process.module').then(m => m.QuizProcessModule) },
+    { path: '', redirectTo: 'process', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
